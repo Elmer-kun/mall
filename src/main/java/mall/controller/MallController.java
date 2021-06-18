5d831796de75c790bd30ea749343e46002884bd5
sunyukun <sunyukun@kyland.com>
2021-06-18 17:57:01 +0800

    增加商品的增删改接口

--- src/main/java/mall/controller/MallController.java
+++ src/main/java/mall/controller/MallController.java
@@ -2,9 +2,16 @@ package mall.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import mall.base.ResponseUtil;
+import mall.entity.mall.MallGoods;
+import mall.mapper.mall.MallGoodsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.Date;
 
 /**
  * @author elmer
@@ -13,12 +20,68 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MallController {
 
+    @Autowired
+    MallGoodsMapper goodsMapper;
+
     @RequestMapping("login")
     public Object doLogin(@RequestBody String data){
         JSONObject json = JSONObject.parseObject(data);
         System.out.println(json.get("userName"));
         System.out.println(json.get("passWord"));
         return ResponseUtil.ok();
+    }
+
+    @RequestMapping("getGoodsList")
+    public Object getGoodsList(){
+        return ResponseUtil.ok(goodsMapper.getAll());
+    }
+
+    @RequestMapping("addGoods")
+    public Object addGoods(@RequestBody MallGoods goods){
+        try {
+            goods.setApproveStatus((byte)1);
+            goods.setBrokerageType((byte)0);
+            goodsMapper.insert(goods);
+        }catch (Exception e){
+            return ResponseUtil.fail(-1, e.getMessage());
+        }
+        return ResponseUtil.ok();
+    }
+
+    @RequestMapping("editGoods")
+    public Object editGoods(@RequestBody MallGoods goods){
+        try {
+            goodsMapper.updateByPrimaryKey(goods);
+        }catch (Exception e){
+            return ResponseUtil.fail(-1, e.getMessage());
+        }
+        return ResponseUtil.ok();
+    }
+
+    @RequestMapping("delGoods")
+    public Object delGoods(@RequestBody JSONObject json){
+        try {
+            goodsMapper.deleteByPrimaryKey((int)json.get("id"));
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseUtil.fail(-1, e.getMessage());
+        }
+        return ResponseUtil.ok();
+    }
+
+    @RequestMapping("goodsPic")
+    public Object saveGoodsPic(@RequestBody MultipartFile file){
+        try{
+            String fileName = (new Date().getTime()) +file.getOriginalFilename();
+//            String path  = System.getProperty("user.dir") + "\\" + fileName;
+            String path  = System.getProperty("user.dir")+"/mallImg/"+(new Date().getTime()) +file.getOriginalFilename();
+            File saveFile = new File(path);
+            file.transferTo(saveFile);
+            return ResponseUtil.ok(fileName);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return ResponseUtil.fail();
     }
 
 }
