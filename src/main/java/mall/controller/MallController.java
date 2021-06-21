2fc44ea64086f36f69e40f0bc33a9f79e902c342
sunyukun <sunyukun@kyland.com>
2021-06-21 16:58:11 +0800

    订单相关接口实现

--- src/main/java/mall/controller/MallController.java
+++ src/main/java/mall/controller/MallController.java
@@ -3,9 +3,10 @@ package mall.controller;
 import com.alibaba.fastjson.JSONObject;
 import mall.base.PasswordUtil;
 import mall.base.ResponseUtil;
-import mall.entity.mall.MallGoods;
-import mall.entity.mall.MallUser;
+import mall.entity.mall.*;
 import mall.mapper.mall.MallGoodsMapper;
+import mall.mapper.mall.MallOrderGoodsMapper;
+import mall.mapper.mall.MallOrderMapper;
 import mall.mapper.mall.MallUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author elmer
@@ -28,6 +30,12 @@ public class MallController {
 
     @Autowired
     MallUserMapper userMapper;
+
+    @Autowired
+    MallOrderMapper orderMapper;
+
+    @Autowired
+    MallOrderGoodsMapper orderGoodsMapper;
 
     @RequestMapping("login")
     public Object doLogin(@RequestBody String data){
@@ -96,6 +104,65 @@ public class MallController {
             return ResponseUtil.ok(fileName);
         }catch (Exception e){
             e.printStackTrace();
+        }
+        return ResponseUtil.fail();
+    }
+
+    @RequestMapping("getOrdersList")
+    public Object getOrdersList(){
+        return ResponseUtil.ok(orderMapper.loadAll());
+    }
+
+    @RequestMapping("sendOrder")
+    public Object sendOrder(@RequestBody JSONObject json){
+
+        try {
+            MallOrder order = orderMapper.selectByPrimaryKey(json.getInteger("orderId"));
+            if(null != order){
+                order.setOrderStatus((short) 301);
+                order.setShipChannel(json.getString("shipChannel"));
+                order.setShipSn(json.getString("shipSn"));
+                order.setShipTime(new Date());
+                orderMapper.updateByPrimaryKey(order);
+                return ResponseUtil.ok();
+            }
+        }catch (Exception e){
+            return ResponseUtil.fail(-1, e.getMessage());
+        }
+        return ResponseUtil.fail();
+    }
+
+    @RequestMapping("orderRefund")
+    public Object orderRefund(@RequestBody JSONObject json){
+        try {
+            MallOrder order = orderMapper.selectByPrimaryKey(json.getInteger("orderId"));
+            if(null != order){
+                System.out.println(json.getString("refundMoney"));
+                return ResponseUtil.ok();
+            }
+        }catch (Exception e){
+            return ResponseUtil.fail(-1, e.getMessage());
+        }
+        return ResponseUtil.fail();
+    }
+
+    @RequestMapping("orderDetail")
+    public Object orderDetail(@RequestBody JSONObject json){
+        try {
+            JSONObject obj = new JSONObject();
+            MallOrder order = orderMapper.selectByPrimaryKey(json.getInteger("id"));
+            if(null != order){
+                MallOrderGoodsExample orderGoodsExample = new MallOrderGoodsExample();
+                orderGoodsExample.createCriteria().andOrderIdEqualTo(order.getId());
+                List<MallOrderGoods> orderGoods = orderGoodsMapper.selectByExample(orderGoodsExample);
+                MallUser user = userMapper.selectByPrimaryKey(order.getUserId());
+                obj.put("order", order);
+                obj.put("user", user);
+                obj.put("orderGoods", orderGoods);
+                return ResponseUtil.ok(obj);
+            }
+        }catch (Exception e){
+            return ResponseUtil.fail(-1, e.getMessage());
         }
         return ResponseUtil.fail();
     }
